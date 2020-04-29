8e591e4bbf902ec5487f490a8edc4f956003b0c7
peterklem <klembczykp@wit.edu>
2020-04-29 15:12:26 -0400

    Added logging and looping
    Implemented a logging system into the GUI, and added a box for each test to control the number of times it is executed.

--- ciate_gui.ps1
+++ ciate_gui.ps1
@@ -10,31 +10,34 @@ function get_address([string]$line){ #Get the path for the executable from the d
     return $path
 }
 
-function get_date(){
-    return Get-Date -Format ShortDatePattern #| Out-String
+function get_date(){ #Used to get date in 
+    return get-date -format "MM/dd/yy" #| Out-String
 }
 
 function get_time(){
-    return Get-Date -Format ShortTimePattern #| Out-String
+    return get-date -format "hh:mm:ss" #| Out-String
 }
 
 #===========================================================
 #    VARIABLES
 #===========================================================
-#$testname_array = @() #holds variable names for each of the created checkboxes
-#$arg_array = @() #holds the variable names for each of the created text boxes
+
 $path_array = @() #Holds the EXE path for each selftest
 $name_array = @() #Holds the name of the test
 $entered_args = @()# Takes the arguments from each text box and holds for use in executable
-$iter = 0 #iterator
+$loop_array = @() # Stores # of loops
+$iter = 0 #iterator 
 $lines = 0 # Num of lines in the doc
 $error_count = 0 # Number of tests with return codes of anything other than 0
 
-#Get number of lines in a text document in variable $doc_length
 Set-Location $PSScriptRoot
-$doc = Get-Content -Path '.\gui_template.txt' #get the text file 
 
-foreach($line in $doc){ #Split the lines in the doc and get the name and executable, as well as # of lines
+# TEXT FILE SAVED HERE
+#--------------------------------------------------------------------------------
+$doc = Get-Content -Path '.\gui_template.txt' #get the text file 
+#--------------------------------------------------------------------------------
+
+foreach($line in $doc){ #Split the lines in the doc and get the name and executable, and get # of lines
     if($line -ne ""){
         $split_line = $line.split(' ')
         $name_array += $split_line[0]
@@ -45,7 +48,7 @@ foreach($line in $doc){ #Split the lines in the doc and get the name and executa
 }
 
 #===========================================================
-#            GUI CREATION
+#            GUI CREATION/SETUP
 #===========================================================
 #------------------------------------------------------------------
 #Start main formS
@@ -59,7 +62,7 @@ $main_form.AutoSize = $true
 # Execute button
 $run_button = New-Object System.Windows.Forms.Button
 $run_button.Text = 'Run Tests'
-$run_button.Location = New-Object System.Drawing.Point(425, 75)
+$run_button.Location = New-Object System.Drawing.Point(550, 75)
 $run_button.Width = 150
 $run_button.Height = 50
 $run_button.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 12, [System.Drawing.FontStyle]::Italic)
@@ -68,12 +71,19 @@ $main_form.Controls.Add($run_button)
 #Logging textbox
 $log_path = New-Object System.Windows.Forms.TextBox
 $log_path.Text = ""
-$log_path.Location = New-Object System.Drawing.Point (530, 40)
+$log_path.Location = New-Object System.Drawing.Point (610, 40)
 $log_path.Width = 300
 #$log_path.ScrollBars = New-Object System.Windows.Forms.ScrollBar("Horizontal")
 $main_form.Controls.Add($log_path)
 
-#Textbox autofiller
+#Textbox autoselect button
+$select_file = New-Object System.Windows.Forms.Button
+$select_file.Text = '...'
+$select_file.Location = New-Object System.Drawing.Point(910, 39)
+$select_file.Width = 30
+$select_file.Height = 22
+$select_file.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 10)
+$main_form.Controls.Add($select_file)
 
 #Test Label
 $testLabel = New-Object System.Windows.Forms.Label
@@ -93,10 +103,19 @@ $argLabel.Width = 100
 $argLabel.Height = 30
 $main_form.Controls.Add($argLabel)
 
+#Looping label
+$loopLabel = New-Object System.Windows.Forms.Label
+$loopLabel.Text = "Loops"
+$loopLabel.Location = New-Object System.Drawing.Point ( 400, 10)
+$loopLabel.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 10, [System.Drawing.FontStyle]::Bold)
+$loopLabel.Width = 100 
+$loopLabel.Height = 30
+$main_form.Controls.Add($loopLabel)
+
 #LogFile path label
 $logLabel = New-Object System.Windows.Forms.Label
 $logLabel.Text = "Log File Path"
-$logLabel.Location = New-Object System.Drawing.Point (425, 40)
+$logLabel.Location = New-Object System.Drawing.Point (530, 40)
 $logLabel.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 10)
 $logLabel.Width = 100 
 $logLabel.Height = 30
@@ -107,63 +126,91 @@ for($i = 0; $i -lt $lines; $i++){
     #titles (saved as test_[i])
     New-Object System.Windows.Forms.Checkbox | `
     Set-Variable -Name ("test_" + $i)
-    #$testname_array += ("$test_" + $i) 
 
     #arg boxes (saved as arguments_[i])
     New-Object System.Windows.Forms.TextBox | `
     Set-Variable -Name ("arguments_" + $i)
-    #$arg_array += ("$arguments_" + $i)
+
+    #loop boxes
+    New-Object System.Windows.Forms.TextBox | `
+    Set-Variable -Name ("loops_" + $i)
 }
 
-#Properties to checkboxes
-[int]$iter = 0 #Spaces out GUI evenly
-Get-Variable -name test_* -ValueOnly | ForEach-Object {
+#Properties for auto-generated checkboxes
+[int]$iter = 0 #Used for GUI spacing
+Get-Variable -name test_* -ValueOnly | ForEach-Object { #Loop through "test" variables
    $_.Text = $name_array[$iter]
    $_.Location = New-Object System.Drawing.Point (30, (38 + 30*$iter))
    $_.Width = 100
    $_.Height = 30
+   $main_form.Controls.Add($_)
    $iter++
 }
 
-#Properties to TextBoxes
-[int]$iter = 0 #Spaces out GUI evenly
-Get-Variable -name arguments_* -ValueOnly | ForEach-Object {
+#Properties for auto-generated argument text boxes
+[int]$iter = 0 #Used for GUI spacing
+Get-Variable -name arguments_* -ValueOnly | ForEach-Object { #Loop through "arguments" variables
    $_.Location = New-Object System.Drawing.Point (130, (40 + 30*$iter))
    $_.Width = 250
    $_.Height = 50
    $_.Visible = $TRUE
+   $main_form.Controls.Add($_)
    $iter++
+   
 }
 
-
-#Add buttons along the x-value of 300
-get-variable -Name test_* -ValueOnly | ForEach-Object {
-    $main_form.Controls.Add($_)
+#Properties for auto-generated looping text boxes
+[int]$iter = 0 #Used for GUI spacing
+Get-Variable -name loops_* -ValueOnly | ForEach-Object { #Loop through "loop" variables
+   $_.Location = New-Object System.Drawing.Point (400, (40 + 30*$iter))
+   $_.Width = 75
+   $_.Height = 50
+   $_.Visible = $TRUE
+   $_.Text = "1"
+   $main_form.Controls.Add($_)
+   $iter++
+   
 }
+<#
+#get-variable -Name test_* -ValueOnly | ForEach-Object {
+    #$main_form.Controls.Add($_)
+#}
 
 Get-Variable -Name arguments_* -ValueOnly | ForEach-Object {
-    $main_form.Controls.Add($_)
+    #$main_form.Controls.Add($_)
 }
+#>
+
 
 
 #--------------------------------------------------------------------
 
 #===================================================================
-#        GUI COMMANDS
+#        GUI COMMANDS/BUTTON PRESSES
 #===================================================================
 
 #---------------------------------------------------------------------
+$select_file.Add_Click({
+    #Add-Type -AssemblyName System.Windows.Forms
+    $f = new-object Windows.Forms.OpenFileDialog
+    $f.InitialDirectory = $PSScriptRoot
+    $f.Filter = "Text Files (*.txt)|*.txt"
+    $f.ShowHelp = $true
+    $f.Multiselect = $false
+    [void]$f.ShowDialog()
+    $log_path.Text = $f.Filename
+})
 
-$run_button.Add_Click({
+$run_button.Add_Click({ # Runs if "Run  
     #check log path
-    if($log_path.Text -eq ""){
-        $log_path.Text = $PSScriptRoot + "\test_log.txt"
+    if($log_path.Text -eq ""){ # If no log path inputted
+        $log_path.Text = $PSScriptRoot + "\test_log.txt" #
     }
 
     #Start Log:
     $time = get_time
     $date = get_date
-    "Test started on " + $date + " at " + $time + "`n"| Out-File -FilePath $log_path.Text -Append
+    "EXECUTION STARTED " + $date + " AT " + $time + "`n"| Out-File -FilePath $log_path.Text -Append
 
     #Get arguments from each text box 
     Get-Variable -Name arguments_* -ValueOnly | ForEach-Object {
@@ -171,26 +218,37 @@ $run_button.Add_Click({
             $entered_args += $holder 
     }
 
+    #Get arguments for each loop box
+    Get-Variable -Name loops_* -ValueOnly | ForEach-Object {
+            $int_holder= $_.Text -as [int]
+            if($int_holder -eq "" -or $int_holder -le 0){ #If loop count not specified or bad value
+                $int_holder = 1
+            }
+            $loop_array += $int_holder #append to array
+    }
+
     #Find selected checkboxes
     $iter = 0
+    
     Get-Variable -name test_* -ValueOnly | ForEach-Object {
         
-        if($_.Checked){
-            #Get arguments from table and run exe
-            Start-Process $path_array[$iter] -ArgumentList $entered_args[$iter] | Wait-Process
-            $return_val = $LASTEXITCODE
-            if($return_val -ne 0){
-                #log failure
-                #$timestamp + "`t" + $name_array[$iter] + " FAILED!" | Add-Content -path $log_path.Text
-                $text_string = "`t" + $name_array[$iter] + " FAILED!" #Get values into one string
-                Add-Content -path $log_path.Text -Value $text_string 
-                "`t" + $name_array[$iter] + " Error Code: " + $return_val | Out-File -FilePath $log_path.Text -Append
+        if($_.Checked){ #Check status of current checkbox
 
-            }else{
-                #log success
-                $text_string = "`t" + $name_array[$iter] + " PASSED!" #Get values into one string
-                Add-Content -path $log_path.Text -Value $text_string
+            for($i = 0; $i -lt $loop_array[$iter]; $i++){ # Check loop array for # of times to run test
+                #Get arguments from table and run exe
+                $process = Start-Process $path_array[$iter] -ArgumentList $entered_args[$iter] -PassThru
+                #$handle = $proc.Handle # cache proc.Handle
+                $process.WaitForExit();
+                $error_code = $process.ExitCode #save exit code
+                
+
+                #Log the returned value to the log file
+                $time = get_time
+                $log_entry = $time + "`t" + $name_array[$iter] + " returned value: " + $error_code 
+                $log_entry | Out-File -FilePath $log_path.Text -Append
             }
+            
+            
         }
         $iter++
     }
@@ -198,4 +256,4 @@ $run_button.Add_Click({
 })
 
 [void]$main_form.ShowDialog()
-# 1 2 selftest "C:\Users\klepeter\Documents\PeterK_Local\CIATE\Powershell GUI\LogFile.txt"
+# 1 2 selftest "C:\Users\klepeter\Documents\PeterK_Local\CIATE\GUI_github\execution_gui\LogFile.txt"
